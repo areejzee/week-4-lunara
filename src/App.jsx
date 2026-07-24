9b68586050e5cc60e9879ba275b87af3eb0edb91
Areej <areejxee@gmail.com>
2026-07-24 16:02:11 +0500

    Wire up global state with Auth, Product, and Cart context providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Shop from './pages/Shop'
-import About from './pages/About'
-import ProductDetails from './pages/ProductDetails'
-import Contact from './pages/Contact'
-import NotFound from './pages/NotFound'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
+import Home from './pages/Home/Home'
+import Shop from './pages/Shop/Shop'
+import About from './pages/About/About'
+import ProductDetails from './pages/ProductDetails/ProductDetails'
+import Contact from './pages/Contact/Contact'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+import Dashboard from './pages/Dashboard/Dashboard'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
   return (
@@ -18,6 +22,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
